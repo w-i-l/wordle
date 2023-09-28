9d40af912452064a7e679c9f9687cc65bba4e33b
Mihai Ocnaru <mihai.ocnaru@mready.net>
2023-09-28 12:41:37 +0300

    initial commit

---
+++ wordle/wordle/wordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordleApp.swift
+//  wordle
+//
+//  Created by Mihai Ocnaru on 28.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct wordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
